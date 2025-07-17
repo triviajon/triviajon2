3dafb08ab53b124900e9a29ad1c1b8b643bfa1e9
Jon Rosario <jonf.rosario@gmail.com>
2025-07-17 18:33:33 -0400

    Update eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,14 @@
-export default function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("assets");
-    eleventyConfig.addPassthroughCopy({ "assets/images/favicon": "/" });
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy({ "assets/images/favicon": "/" });
 
-    return {
-      pathPrefix: "/triviajon2/",
-      dir: {
-        input: "./pages",
-        output: "./_site",
-        layouts: "../layouts",
-      },
-    };
-  }
+  return {
+    pathPrefix: "/triviajon2/",
+    dir: {
+      input: "./pages",
+      output: "./_site",
+      layouts: "../layouts",
+    },
+  };
+}
