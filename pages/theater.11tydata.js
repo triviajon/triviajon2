58cd4bbbc360b012735f2e6c90c8582b73705a44
Jon Rosario <jonf.rosario@gmail.com>
2025-12-20 19:29:01 -0500

    Automate creation of entries on theater page; fix button styling

---
+++ pages/theater.11tydata.js
@@ -0,0 +1,157 @@
+const productions = [
+  {
+    title: "A New Brain",
+    year: "2025",
+    org: "St Bart's Players",
+    roles: "Tech Load-in (Sets & Lights)",
+    image: "/assets/images/theater_thumbnails/st_barts.png",
+    alt: "St Bart's Players",
+    categories: ["lights", "other"],
+  },
+
+  {
+    title: "A Gentleman's Guide to Love & Murder",
+    year: "2025",
+    org: "MIT Musical Theatre Guild",
+    roles: "Lighting Designer, Master Electrician",
+    image: "/assets/images/theater_thumbnails/gentlemans_guide.jpg",
+    alt: "A Gentleman's Guide to Love & Murder",
+    categories: ["lights"],
+  },
+
+  {
+    title: "Those Who Tried To Touch The Sky",
+    year: "2025",
+    org: "MIT Life on Stage Theatre",
+    roles:
+      "Master Electrician, Assistant Lighting Designer, Assistant Technical Director",
+    image: "/assets/images/theater_thumbnails/touch_the_sky.jpg",
+    alt: "Those Who Tried To Touch The Sky",
+    categories: ["lights", "other"],
+  },
+
+  {
+    title: "Cabaret",
+    year: "2025",
+    org: "MIT Musical Theatre Guild",
+    roles: "Lighting Designer, Producing Team",
+    image: "/assets/images/theater_thumbnails/cabaret.jpg",
+    alt: "Cabaret",
+    categories: ["lights", "other"],
+  },
+
+  {
+    title: "Assassins",
+    year: "2024",
+    org: "MIT Musical Theatre Guild",
+    roles:
+      "Cast (Charles J. Guiteau), Assistant Lighting Designer, Master Electrician, Carpenter",
+    image: "/assets/images/theater_thumbnails/assassins.jpg",
+    alt: "Assassins",
+    categories: ["cast", "lights", "other"],
+  },
+
+  {
+    title: "The Importance of Being Earnest",
+    year: "2024",
+    org: "MIT Life on Stage Theatre",
+    roles: "Carpenter",
+    image: "/assets/images/theater_thumbnails/earnest.jpg",
+    alt: "The Importance of Being Earnest",
+    categories: ["other"],
+  },
+
+  {
+    title: "Legally Blonde",
+    year: "2024",
+    org: "MIT Next Act",
+    roles: "Vocal Director",
+    image: "/assets/images/theater_thumbnails/legally_blonde.jpg",
+    alt: "Legally Blonde",
+    categories: ["other"],
+  },
+
+  {
+    title: "The Lightning Thief",
+    year: "2024",
+    org: "MIT Musical Theatre Guild",
+    roles: "Electrician",
+    image: "/assets/images/theater_thumbnails/lightning_thief.jpg",
+    alt: "The Lightning Thief",
+    categories: ["lights"],
+  },
+
+  {
+    title: "Chicago",
+    year: "2024",
+    org: "MIT Musical Theatre Guild",
+    roles: "Assistant Producer, Cast (Amos Hart)",
+    image: "/assets/images/theater_thumbnails/chicago.jpg",
+    alt: "Chicago",
+    categories: ["cast", "other"],
+  },
+
+  {
+    title: "Stupid Fucking Bird",
+    year: "2023",
+    org: "MIT Life on Stage Theatre",
+    roles: "Dev",
+    image: "/assets/images/theater_thumbnails/sfb.jpg",
+    alt: "Stupid Fucking Bird",
+    categories: ["cast"],
+  },
+
+  {
+    title: "Remembering Her",
+    year: "2023",
+    org: "MIT Next Act",
+    roles: "Cast (Dave)",
+    image: "/assets/images/theater_thumbnails/remembering_her.jpg",
+    alt: "Remembering Her",
+    categories: ["cast"],
+  },
+
+  {
+    title: "Heathers",
+    year: "2023",
+    org: "MIT Musical Theatre Guild",
+    roles: "Cast (Kurt's Dad)",
+    image: "/assets/images/theater_thumbnails/heathers.jpg",
+    alt: "Heathers",
+    categories: ["cast"],
+  },
+
+  {
+    title: "Pippin",
+    year: "2022",
+    org: "MIT Musical Theatre Guild",
+    roles: "Ensemble",
+    image: "/assets/images/theater_thumbnails/pippin.jpg",
+    alt: "Pippin",
+    categories: ["cast"],
+  },
+
+  {
+    title: "Timon of Athens",
+    year: "2021",
+    org: "MIT Shakespeare Ensemble",
+    roles: "Video Editor",
+    image: "/assets/images/theater_thumbnails/timon.png",
+    alt: "Timon of Athens",
+    categories: ["other"],
+  },
+
+  {
+    title: "Into the Woods",
+    year: "2020",
+    org: "Dwight Morrow High School",
+    roles: "Cast (Mysterious Man)",
+    image: "/assets/images/theater_thumbnails/woods.jpg",
+    alt: "Into the Woods",
+    categories: ["cast"],
+  },
+];
+
+export default {
+  productions,
+};
